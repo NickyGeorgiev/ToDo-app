065d5fd0093ef885ebfe40856b3ce7d0fed21a83
Nikolay Georgiev <n.georrgiev@gmail.com>
2022-07-17 15:16:27 +0300

    add todo form

---
+++ src/components/InitialTodo.js
@@ -0,0 +1,16 @@
+export const InitialTodo = () => {
+    return (
+        <>
+            <div>ADD ToDo</div>
+                <form className="add-todo">
+                    <label htmlFor="title">ToDo Name *</label><br />
+                    <input type="text" id="title" name="title" /><br />
+                    <label htmlFor="subtitle">ToDo Subtitle</label><br />
+                    <input type="text" id="subtitle" name="subtitle" /><br />
+                    <label htmlFor="subtitle">ToDo Description</label><br />
+                    <textarea type="textarea" id="description" name="description" /><br />
+                    <input className="submit" type="submit" value="Add new ToDo" />
+                </form>
+        </>
+    )
+}
